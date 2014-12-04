cca2beb250b047b189e1810ab5f3fe360454082f
hugolpz <hugo.lpz@gmail.com>
2014-12-04 22:58:13 +0100

    test

--- topojson_test.mk
+++ topojson_test.mk
@@ -1,9 +1,13 @@
-countries_fix:
+countries_fix: delete
 	./topojson/bin/topojson \
-#	./.bin/topojson \
 		--id-property none \
 		-p name=NAME \
 		-q 1e4 \
 		--filter=small \
 		-o countries.geo.json \
 		-- admin_0=countries.tmp.geo.json
+
+#	./.bin/topojson \
+
+delete:
+	rm -f countries.geo.json
